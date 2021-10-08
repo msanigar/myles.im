aef6d35b797a382f8dfa95a7fe539fb007e08272
Myles <myles.sanigar@gmail.com>
2021-10-08 21:57:04 +0100

    add netlify & netlify cms

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
-module.exports = {
-  reactStrictMode: true,
-}
+module.exports = ({
+  pageExtensions: ["tsx"],
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.yml$/,
+          type: "json",
+          use: "yaml-loader",
+        },
+        {
+          test: /\.svg$/,
+          use: "@svgr/webpack",
+        },
+      ]
+    );
+    return config;
+  },
+});
