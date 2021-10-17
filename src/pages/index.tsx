32459eb1b2c32a3aca27e64bc165c9f1f9d10428
Myles <myles.sanigar@gmail.com>
2021-10-17 12:19:25 +0100

    whatsapp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,7 @@ export default function Index() {
         </div>
       </section>
       <div className="elfsight-app-c1fa2086-e756-42db-a712-53f91b5195db"></div>
+      <div className="elfsight-app-7d1ce7c1-caf3-42e7-ad0e-ee3d7807f02e"></div>
     </Layout>
   );
 }
