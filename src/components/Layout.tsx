ce7a44f249b83ec30f5b34a69db31e86ecd93c77
Myles <myles.sanigar@gmail.com>
2021-10-17 11:20:58 +0100

    test elfsight

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -33,6 +33,7 @@ export default function Layout({ children }: Props) {
             data-domain="myles.im"
             src="https://plausible.io/js/plausible.js"
           ></script>
+          <script src="https://apps.elfsight.com/p/platform.js" defer></script>
           <script
             src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"
             crossOrigin="anonymous"
