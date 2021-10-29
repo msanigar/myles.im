02f7de3662d7b462bcd736a209d3e1b32abf714d
Myles <myles.sanigar@gmail.com>
2021-10-29 21:33:51 +0100

    strip out unneeded stuff

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,7 +2,6 @@ import Layout from '../../components/Layout';
 import BasicMeta from '../../components/meta/BasicMeta';
 import OpenGraphMeta from '../../components/meta/OpenGraphMeta';
 import TwitterCardMeta from '../../components/meta/TwitterCardMeta';
-import Team from '../../components/svgs/Team';
 
 type Props = {};
 export default function Index({}: Props) {
@@ -13,13 +12,6 @@ export default function Index({}: Props) {
       <BasicMeta url={url} title={title} />
       <OpenGraphMeta url={url} title={title} />
       <TwitterCardMeta url={url} title={title} />
-      <div className="container">
-        <div className="columns is-centered">
-          <div className="column is-half">
-            <Team />
-          </div>
-        </div>
-      </div>
       <div className="content p-4">
         <div className="block has-text-centered">
           <h1 className="title">Who are we?</h1>
@@ -38,22 +30,6 @@ export default function Index({}: Props) {
           porttitor eget nunc et, tincidunt porta odio. Quisque auctor nunc
           neque, at cursus libero fringilla nec. Pellentesque a tellus faucibus,
           bibendum sapien eget, congue nunc.
-        </p>
-      </div>
-      <div className="block p-4 mb-4">
-        <p>
-          Cras sed urna eleifend, viverra sapien non, rhoncus dui. Morbi finibus
-          dignissim sem. Mauris sed condimentum velit. Vestibulum fermentum enim
-          et elit finibus, nec molestie tortor ultricies. Pellentesque bibendum
-          eget velit vitae sagittis. Nullam feugiat finibus egestas. Morbi
-          suscipit justo ullamcorper lacus molestie, eu eleifend turpis
-          scelerisque. Donec lectus nibh, fringilla nec maximus ac, facilisis ac
-          sem. Morbi vitae nulla eget diam cursus fringilla. Duis lectus ipsum,
-          placerat in velit sed, bibendum blandit mi. Suspendisse ac nisl
-          viverra, dignissim felis ac, tincidunt metus. Integer quis
-          pellentesque nunc. Maecenas blandit justo dui, eget lacinia ipsum
-          fringilla sed. Maecenas commodo commodo justo, in rutrum massa. In
-          scelerisque feugiat nibh in mattis.
         </p>
       </div>
     </Layout>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,6 @@ import Layout from '../components/Layout';
 import BasicMeta from '../components/meta/BasicMeta';
 import OpenGraphMeta from '../components/meta/OpenGraphMeta';
 import TwitterCardMeta from '../components/meta/TwitterCardMeta';
-import config from '../lib/config';
-import Carousel from '../components/carousel/Carousel';
-
-const heroStyle = {
-  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.7)), url(${config?.header})`,
-  backgroundPosition: 'center',
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat',
-};
-
-const SLIDE_COUNT = config.feature_images?.length;
-const slides = Array.from(Array(SLIDE_COUNT).keys());
 
 export default function Index() {
   return (
@@ -21,10 +9,7 @@ export default function Index() {
       <BasicMeta url={'/'} />
       <OpenGraphMeta url={'/'} />
       <TwitterCardMeta url={'/'} />
-      <section
-        style={config?.header ? heroStyle : null}
-        className="hero is-success is-large"
-      >
+      <section className="hero">
         <div className="hero-body">
           <div className="container has-text-centered">
             <p className="title">myles.im ~ work in progress</p>
@@ -32,14 +17,6 @@ export default function Index() {
           </div>
         </div>
       </section>
-      <div>
-        {config.feature_images?.length > 0 && <Carousel slides={slides} />}
-      </div>
-      <div className="container">
-        <div className="elfsight-app-c1fa2086-e756-42db-a712-53f91b5195db"></div>
-        <div className="elfsight-app-7d1ce7c1-caf3-42e7-ad0e-ee3d7807f02e"></div>
-        <div className="elfsight-app-59bdb7b0-0f06-4f81-b9f5-24d3ef875a73"></div>
-      </div>
     </Layout>
   );
 }
