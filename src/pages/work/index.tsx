ff53e306ae5e2255492a685191944d928498ded4
Myles <myles.sanigar@gmail.com>
2021-10-31 14:38:07 +0000

    more shine

--- src/pages/work/index.tsx
+++ src/pages/work/index.tsx
@@ -14,74 +14,174 @@ export default function Index({}: Props) {
       <OpenGraphMeta url={url} title={title} />
       <TwitterCardMeta url={url} title={title} />
       <div className="container work">
-        <div className="content p-4">
-          <div className="block has-text-centered">
-            <h1 className="title">Portfolio & Work</h1>
+        <section className="pb-6">
+          <div className="content p-4">
+            <div className="block has-text-centered">
+              <h1 className="title">Portfolio & Work</h1>
+            </div>
+            <h2 className="subtitle">This website</h2>
+            <p>
+              This website has been migrated over to the{' '}
+              <strong>
+                <a
+                  className="has-text-danger dotted-underline"
+                  href="https://www.netlify.com/jamstack"
+                  target="_blank"
+                >
+                  Jamstack
+                </a>
+              </strong>
+              , away from more traditional VPS/LAMP based hosting and older
+              client-side technologies. At the root of it all is Netlify, a
+              cloud-based hosting and CI/CD SaaS platform. Netlify CMS is
+              powering the blog posts and some of the re-usable config such as
+              meta-data.
+            </p>
+            <p>
+              NextJS is powering the NodeJS static-generation and client-side,
+              utilising React and TypeScript for the functionality and Bulma CSS
+              & hand-rolled Sass for the styling. Cloudinary CDN has been used
+              to deliver images from the edge; lazy-loaded, optimized and
+              cached.
+            </p>
+            <p>
+              Google fonts, FontAwesome, unDraw illustrations, Unsplash images
+              and Plausible analytics are the cherry on top.
+            </p>
           </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            orci odio, auctor in commodo et, posuere sit amet neque. Donec nec
-            auctor ante. Mauris interdum neque ac rutrum consequat. Nunc urna
-            lorem, vehicula in molestie id, imperdiet sed nibh. Nunc blandit
-            iaculis urna ut fermentum. Vivamus quis purus imperdiet, tincidunt
-            mi id, dictum urna. Praesent quis ex vel lectus dapibus accumsan.
-            Donec nec ipsum eu elit gravida efficitur gravida sed neque. Sed
-            sagittis purus non tincidunt lobortis. Donec consequat luctus diam.
-            Duis ut arcu odio. Vivamus gravida sit amet nunc at vulputate.
-            Aliquam porttitor tristique mauris, ut convallis sem scelerisque in.
-            Mauris sem odio, porttitor eget nunc et, tincidunt porta odio.
-            Quisque auctor nunc neque, at cursus libero fringilla nec.
-            Pellentesque a tellus faucibus, bibendum sapien eget, congue nunc.
-          </p>
-        </div>
-
-        <div className="tile is-ancestor">
-          <div className="tile is-vertical is-8">
-            <div className="tile">
-              <div className="tile is-parent is-vertical">
-                <article className="tile is-child notification is-primary">
-                  <p className="title">Vertical...</p>
-                  <p className="subtitle">Top tile</p>
-                </article>
-                <article className="tile is-child notification is-warning">
-                  <p className="title">...tiles</p>
-                  <p className="subtitle">Bottom tile</p>
-                </article>
+        </section>
+        <section className="pb-6">
+          <div className="content p-4">
+            <h2 className="subtitle">Projects & Freelance</h2>
+            <p>Lorem</p>
+          </div>
+          <div className="tile is-ancestor">
+            <div className="tile is-vertical is-8">
+              <div className="tile">
+                <div className="tile is-parent is-vertical">
+                  <article className="tile is-child notification is-primary">
+                    <p className="title">Vertical...</p>
+                    <p className="subtitle">Top tile</p>
+                  </article>
+                  <article className="tile is-child notification is-warning">
+                    <p className="title">...tiles</p>
+                    <p className="subtitle">Bottom tile</p>
+                  </article>
+                </div>
+                <div className="tile is-parent">
+                  <article className="tile is-child notification is-info">
+                    <a
+                      target="_blank"
+                      href="https://eloquent-brattain-42e403.netlify.app/"
+                    >
+                      <p className="title">Surgo.gg</p>
+                      <p className="subtitle">Check it out!</p>
+                      <figure className="image is-4by3">
+                        <img src="https://myles.im/images/kalen-emsley-photo-1464822759023-fed622ff2c3b.jpg" />
+                      </figure>
+                    </a>
+                  </article>
+                </div>
               </div>
               <div className="tile is-parent">
-                <article className="tile is-child notification is-info">
-                  <a
-                    target="_blank"
-                    href="https://eloquent-brattain-42e403.netlify.app/"
-                  >
-                    <p className="title">Surgo.gg</p>
-                    <p className="subtitle">Check it out!</p>
-                    <figure className="image is-4by3">
-                      <img src="https://myles.im/images/kalen-emsley-photo-1464822759023-fed622ff2c3b.jpg" />
-                    </figure>
-                  </a>
+                <article className="tile is-child notification is-danger">
+                  <p className="title">Wide tile</p>
+                  <p className="subtitle">Aligned with the right tile</p>
+                  <div className="content"></div>
                 </article>
               </div>
             </div>
             <div className="tile is-parent">
-              <article className="tile is-child notification is-danger">
-                <p className="title">Wide tile</p>
-                <p className="subtitle">Aligned with the right tile</p>
-                <div className="content"></div>
+              <article className="tile is-child notification is-success">
+                <div className="content">
+                  <p className="title">Tall tile</p>
+                  <p className="subtitle">With even more content</p>
+                  <div className="content"></div>
+                </div>
               </article>
             </div>
           </div>
-          <div className="tile is-parent">
-            <article className="tile is-child notification is-success">
-              <div className="content">
-                <p className="title">Tall tile</p>
-                <p className="subtitle">With even more content</p>
-                <div className="content"></div>
-              </div>
-            </article>
+        </section>
+
+        <section className="pb-6">
+          <div className="content p-4">
+            <h2 className="subtitle">Career</h2>
+            <p>Lorem</p>
           </div>
-        </div>
+          <div className="timeline is-centered">
+            <header className="timeline-header">
+              <span className="tag is-medium is-info">2013</span>
+            </header>
+            <div className="timeline-item">
+              <div className="timeline-marker is-primary"></div>
+              <div className="timeline-content">
+                <p className="heading">
+                  <b>boohoo.com</b>
+                </p>
+                <p>Frontend Developer</p>
+              </div>
+            </div>
+            <header className="timeline-header">
+              <span className="tag is-info">2016</span>
+            </header>
+            <div className="timeline-item">
+              <div className="timeline-marker is-primary"></div>
+              <div className="timeline-content">
+                <p className="heading">
+                  <b>ao.com</b>
+                </p>
+                <p>Frontend Developer</p>
+              </div>
+            </div>
+            <header className="timeline-header">
+              <span className="tag is-info">2017</span>
+            </header>
+            <div className="timeline-item">
+              <div className="timeline-marker is-primary is-icon">
+                <i className="fa fa-flag"></i>
+              </div>
+              <div className="timeline-content">
+                <p className="heading">
+                  <b>missguided.com</b>
+                </p>
+                <p>Senior Software Engineer</p>
+                <p>Software Engineer</p>
+              </div>
+            </div>
+            <header className="timeline-header">
+              <span className="tag is-info">2019</span>
+            </header>
+            <div className="timeline-item">
+              <div className="timeline-marker is-primary is-icon">
+                <i className="fa fa-flag"></i>
+              </div>
+              <div className="timeline-content">
+                <p className="heading">
+                  <b>ivendi ltd.</b>
+                </p>
+                <p>Principal Engineer</p>
+                <p>Tech Lead</p>
+              </div>
+            </div>
+            <header className="timeline-header">
+              <span className="tag is-info">2020</span>
+            </header>
+            <div className="timeline-item is-danger">
+              <div className="timeline-marker is-danger"></div>
+              <div className="timeline-content">
+                <p className="heading">
+                  <b>asos.com</b>
+                </p>
+                <p>Tech Lead</p>
+              </div>
+            </div>
+            <header className="timeline-header">
+              <span className="tag is-medium is-danger">today</span>
+            </header>
+          </div>
+        </section>
       </div>
     </Layout>
   );
 }
+7;
