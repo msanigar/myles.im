73d1a07c0f1c286d245be2a6a50140ef05c5664b
Myles <myles.sanigar@gmail.com>
2024-09-02 10:38:19 +0100

    nested nested

---
+++ src/pages/sandbox/nested/index.tsx
@@ -0,0 +1,15 @@
+type Props = {};
+export default function Index({}: Props) {
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src="https://beadlamtractor.co.uk/sandbox/youtube-nocookie/"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      allowFullScreen
+    ></iframe>
+  );
+}
