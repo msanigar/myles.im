b83546ecf9149cd80ee3a66f6fb34fff5abf633a
Myles <myles.sanigar@gmail.com>
2024-09-02 10:16:02 +0100

    nested

---
+++ src/pages/sandbox/youtube/index.tsx
@@ -0,0 +1,15 @@
+type Props = {};
+export default function Index({}: Props) {
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/LXb3EKWsInQ?si=01lr_qXrM8w4dHSz"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      allowFullScreen
+    ></iframe>
+  );
+}
