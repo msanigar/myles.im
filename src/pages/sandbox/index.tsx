9aaa95cc82054d81d736072976ff2cbc245ae4b7
Myles <myles.sanigar@gmail.com>
2024-09-02 10:07:00 +0100

    playing around

---
+++ src/pages/sandbox/index.tsx
@@ -0,0 +1,13 @@
+type Props = {};
+export default function Index({}: Props) {
+  return (
+    <iframe
+      width="1440"
+      height="762"
+      src="https://www.youtube-nocookie.com/embed/LXb3EKWsInQ"
+      frameBorder="0"
+      allow="autoplay; encrypted-media"
+      allowFullScreen
+    ></iframe>
+  );
+}
